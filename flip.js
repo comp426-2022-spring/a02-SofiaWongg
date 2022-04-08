4633d3ccea16e80084365c262656604ebf80da8f
Sofia Wong <ssofia@ad.unc.edu>
2022-04-08 15:48:56 -0400

    am i commiting

--- flip.js
+++ flip.js
@@ -1,6 +1,5 @@
 // Import the coinFlip function from your coin.mjs file
 import { coinFlip, coinFlips} from './modules/coin.mjs';
-import minimist from "minimist";
 
 // Call the coinFlip function and put the return into STDOUT
 console.log(coinFlip());
