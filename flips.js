734dc3d648bbc03ff149e42fdeb4699e03bfa448
Sofia Wong <ssofia@ad.unc.edu>
2022-04-08 15:46:54 -0400

    ,m

--- flips.js
+++ flips.js
@@ -1,19 +1,9 @@
 import{countFlips, coinFlips} from './modules/coinFlips.mjs'
 import minimist from "minimist";
-import {createRequire} from 'module';
 
-var require = createRequire(import.meta.url);
-let argv = minimist(process.argv.slice(2));
-let number = argv.number;
-var flips;
-
-if(number != null){
-    flips = coinFlips(number);
-}
-else {
-    flips = coinFlips(1);
-    
-    // console.log(countFlips(flips));
-}
-console.log(flips);
-console.log(countFlips(flips));
+const args = minimist(process.argv.slice(2))
+args['number']
+const num = args.number || 1
+let flips = coinFlips(num)
+console.log(flips)
+console.log(countFlips(flips))
