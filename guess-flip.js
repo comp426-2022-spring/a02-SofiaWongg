3fea0d4fdb20d88a42a29f6689ef6ea67d6792c4
Sofia Wong <ssofia@ad.unc.edu>
2022-04-08 16:24:00 -0400

    flip

--- guess-flip.js
+++ guess-flip.js
@@ -1,20 +1,18 @@
-import { countFlips, flipACoin } from "./modules/coin.mjs";
-import minimist from "minimist";
+import{coinFlip, flipACoin} from "./modules/coin.mjs"
+import minimist from "minimist"
 import { createRequire } from 'module';
-
 const require = createRequire(import.meta.url);
 
-const argv = minimist(process.argv.slice(2));
-var predict = argv["call"];
-if(predict == null){
-    console.error("Error: no input.")
+const args = minimist(process.argv.slice(2))
+args['call']
+const guess = args.call
+if(guess == null){
+    console.error("Error: no input")
     console.log("Usage: node guess-flip --call=[heads|tails]")
 }
-
-else if(guess != 'heads' || guess != 'tails'){
+else if(!(guess === 'heads' || guess === 'tails')){
     console.log("Usage: node guess-flip --call=[heads|tails]")
 }
 else{
     console.log(flipACoin(guess))
 }
-
